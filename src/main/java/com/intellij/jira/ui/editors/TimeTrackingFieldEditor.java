e03f68a1918f71254811d9a51692e19f037ef19d
aromano <aromano@vectoritcgroup.com>
2018-10-30 10:34:08 +0100

    Timetracking field support

--- src/main/java/com/intellij/jira/ui/editors/TimeTrackingFieldEditor.java
+++ src/main/java/com/intellij/jira/ui/editors/TimeTrackingFieldEditor.java
@@ -2,12 +2,19 @@ package com.intellij.jira.ui.editors;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBTextField;
 import com.intellij.util.ui.FormBuilder;
+import com.intellij.util.ui.UI;
 
 import javax.swing.*;
 import java.util.Map;
+
+import static com.intellij.openapi.util.text.StringUtil.isEmpty;
+import static com.intellij.openapi.util.text.StringUtil.isNotEmpty;
+import static java.util.Objects.nonNull;
 
 public class TimeTrackingFieldEditor extends AbstractFieldEditor {
 
@@ -27,9 +34,11 @@ public class TimeTrackingFieldEditor extends AbstractFieldEditor {
     @Override
     public JComponent createPanel() {
         this.myFirstField = new JBTextField();
+        this.myFirstField.setPreferredSize(UI.size(255, 24));
 
         this.mySecondLabel = new JBLabel(REMAINING_ESTIMATE_FIELD);
         this.mySecondField = new JBTextField();
+        this.mySecondField.setPreferredSize(UI.size(255, 24));
 
 
         return FormBuilder.createFormBuilder()
@@ -40,14 +49,36 @@ public class TimeTrackingFieldEditor extends AbstractFieldEditor {
 
     @Override
     public Map<String, String> getInputValues() {
-        myInputValues.put(ORIGINAL_ESTIMATE_FIELD, myFirstField.getText());
-        myInputValues.put(REMAINING_ESTIMATE_FIELD, mySecondField.getText());
+        myInputValues.put(ORIGINAL_ESTIMATE_FIELD, getOriginalEstimate());
+        myInputValues.put(REMAINING_ESTIMATE_FIELD, getRemainingEstimate());
 
         return myInputValues;
     }
 
     @Override
     public JsonElement getJsonValue() {
-        return JsonNull.INSTANCE;
+        if(isEmpty(getOriginalEstimate()) && isEmpty(getRemainingEstimate())) {
+            return JsonNull.INSTANCE;
+        }
+
+        JsonObject timeObject = new JsonObject();
+        if(isNotEmpty(getOriginalEstimate())){
+            timeObject.add("originalEstimate", new JsonPrimitive(getOriginalEstimate()));
+        }
+
+        if(isNotEmpty(getRemainingEstimate())){
+            timeObject.add("remainingEstimate", new JsonPrimitive(getRemainingEstimate()));
+        }
+
+        return timeObject;
+    }
+
+
+    private String getOriginalEstimate(){
+        return nonNull(myFirstField) ? myFirstField.getText() : "";
+    }
+
+    private String getRemainingEstimate(){
+        return nonNull(mySecondField) ? mySecondField.getText() : "";
     }
 }
